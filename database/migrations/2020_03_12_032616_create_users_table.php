dcaa2a53a1b5786b96f627ee99f9c012b828bc5c
dungpham <dung.pham@asiantech.vn>
2020-03-12 10:31:29 +0700

    create user table

---
+++ database/migrations/2020_03_12_032616_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username');
+            $table->string('password');
+            $table->string('email')->unique();
+            $table->string('image_url')->nullable($value = true);
+            $table->string('address');
+            $table->tinyInteger('role')->nullable($value = true);
+            $table->timestamps();
+        });
+    }
+    //
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
