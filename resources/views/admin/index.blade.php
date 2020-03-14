0601f59cec671b92518328ed7e7ad1ff43126d2b
dungpham <dung.pham@asiantech.vn>
2020-03-14 18:53:05 +0700

    template admin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,222 @@
+
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>@yield('title', 'Admin')</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon
+        ============================================ -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+    <!-- Google Fonts
+        ============================================ -->
+    <link href="https://fonts.googleapis.com/css?family=Play:400,700" rel="stylesheet">
+    <!-- Bootstrap CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <!-- Bootstrap CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+    <!-- owl.carousel CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.theme.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.transitions.css') }}">
+    <!-- animate CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+    <!-- normalize CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
+    <!-- meanmenu icon CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/meanmenu.min.css') }}">
+    <!-- main CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <!-- morrisjs CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/morrisjs/morris.css') }}">
+    <!-- mCustomScrollbar CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/scrollbar/jquery.mCustomScrollbar.min.css') }}">
+    <!-- metisMenu CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/metisMenu/metisMenu.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/metisMenu/metisMenu-vertical.css') }}">
+    <!-- calendar CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/calendar/fullcalendar.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/calendar/fullcalendar.print.min.css') }}">
+    <!-- style CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+    <!-- responsive CSS
+        ============================================ -->
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+    <!-- modernizr JS
+        ============================================ -->
+    <script src="{{ asset('js/vendor/modernizr-2.8.3.min.js') }}"></script>
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+    <div class="left-sidebar-pro">
+        <nav id="sidebar" class="">
+            <div class="sidebar-header">
+                <a href="#"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+                <strong><img src="img/logo/logosn.png" alt="" /></strong>
+            </div>
+            <div class="left-custom-menu-adp-wrap comment-scrollbar">
+                <nav class="sidebar-nav left-sidebar-menu-pro">
+                    <ul class="metismenu" id="menu1">
+                        <li class="active">
+                            <a class="has-arrow" href="#">
+                                   <i class="fa big-icon fa-home icon-wrap"></i>
+                                   <span class="mini-click-non">Ecommerce</span>
+                                </a>
+                            <ul class="submenu-angle" aria-expanded="true">
+                                <li><a title="Product List" href="#"><i class="fa fa-location-arrow sub-icon-mg" aria-hidden="true"></i> <span class="mini-sub-pro">Product List</span></a></li>
+                                <li><a title="User List" href="#"><i class="fa fa-location-arrow sub-icon-mg" aria-hidden="true"></i> <span class="mini-sub-pro">User List</span></a></li>
+                                <li><a title="Category List" href="#"><i class="fa fa-location-arrow sub-icon-mg" aria-hidden="true"></i> <span class="mini-sub-pro">Category List</span></a></li>
+                                <li><a title="Comment List" href="#"><i class="fa fa-location-arrow sub-icon-mg" aria-hidden="true"></i> <span class="mini-sub-pro">Comment List</span></a></li>
+                                <li><a title="Cart List" href="#"><i class="fa fa-location-arrow sub-icon-mg" aria-hidden="true"></i> <span class="mini-sub-pro">Cart List</span></a></li>
+                                <li><a title="Bill List" href="#"><i class="fa fa-location-arrow sub-icon-mg" aria-hidden="true"></i> <span class="mini-sub-pro">Bill List</span></a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+    </div>
+    <!-- Start Welcome area -->
+    <div class="all-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="logo-pro">
+                        <a href="{{ route('admin.index') }}"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-advance-area">
+            <div class="header-top-area">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="header-top-wraper">
+                                <div class="row">
+                                    <div class="col-lg-1 col-md-0 col-sm-1 col-xs-12">
+                                        <div class="menu-switcher-pro">
+                                            <button type="button" id="sidebarCollapse" class="btn bar-button-pro header-drl-controller-btn btn-info navbar-btn">
+                                                    <i class="fa fa-bars"></i>
+                                                </button>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-7 col-sm-6 col-xs-12">
+                                        <div class="header-top-menu tabl-d-n">
+                                            <ul class="nav navbar-nav mai-top-nav">
+                                                <li class="nav-item"><a href="#" class="nav-link">Home</a>
+                                                </li>
+                                                <li class="nav-item"><a href="#" class="nav-link">About</a>
+                                                </li>
+                                                <li class="nav-item"><a href="#" class="nav-link">Services</a>
+                                                </li>
+                                                <li class="nav-item"><a href="#" class="nav-link">Support</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                                        <div class="header-right-info">
+                                            <ul class="nav navbar-nav mai-top-nav header-right-menu">
+                                                <li class="nav-item">
+                                                    <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle">
+                                                            <i class="fa fa-user adminpro-user-rounded header-riht-inf" aria-hidden="true"></i>
+                                                            <span class="admin-name">Advanda Cro</span>
+                                                            <i class="fa fa-angle-down adminpro-icon adminpro-down-arrow"></i>
+                                                        </a>
+                                                    <ul role="menu" class="dropdown-header-top author-log dropdown-menu animated zoomIn">
+                                                        <li><a href="#"><span class="fa fa-user author-log-ic"></span>My Profile</a>
+                                                        </li>
+                                                        <li><a href="#"><span class="fa fa-lock author-log-ic"></span>Log Out</a>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Single pro tab start-->
+
+        @yield('content')
+
+        <!-- Single pro tab End-->
+        <!-- Single pro tab review Start-->
+
+    <!-- jquery
+        ============================================ -->
+    <script src="{{ asset('js/vendor/jquery-1.11.3.min.js') }}"></script>
+    <!-- bootstrap JS
+        ============================================ -->
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <!-- wow JS
+        ============================================ -->
+    <script src="{{ asset('js/wow.min.js') }}"></script>
+    <!-- price-slider JS
+        ============================================ -->
+    <script src="{{ asset('js/jquery-price-slider.js') }}"></script>
+    <!-- meanmenu JS
+        ============================================ -->
+    <script src="{{ asset('js/jquery.meanmenu.js') }}"></script>
+    <!-- owl.carousel JS
+        ============================================ -->
+    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+    <!-- sticky JS
+        ============================================ -->
+    <script src="{{ asset('js/jquery.sticky.js') }}"></script>
+    <!-- scrollUp JS
+        ============================================ -->
+    <script src="{{ asset('js/jquery.scrollUp.min.js') }}"></script>
+    <!-- mCustomScrollbar JS
+        ============================================ -->
+    <script src="{{ asset('js/scrollbar/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+    <script src="{{ asset('js/scrollbar/mCustomScrollbar-active.js') }}"></script>
+    <!-- metisMenu JS
+        ============================================ -->
+    <script src="{{ asset('js/metisMenu/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('js/metisMenu/metisMenu-active.js') }}"></script>
+    <!-- morrisjs JS
+        ============================================ -->
+    <script src="{{ asset('js/sparkline/jquery.sparkline.min.js') }}"></script>
+    <script src="{{ asset('js/sparkline/jquery.charts-sparkline.js') }}"></script>
+    <!-- calendar JS
+        ============================================ -->
+    <script src="{{ asset('js/calendar/moment.min.js') }}"></script>
+    <script src="{{ asset('js/calendar/fullcalendar.min.js') }}"></script>
+    <script src="{{ asset('js/calendar/fullcalendar-active.js') }}"></script>
+    <!-- tab JS
+        ============================================ -->
+    <script src="{{ asset('js/tab.js') }}"></script>
+    <!-- plugins JS
+        ============================================ -->
+    <script src="{{ asset('js/plugins.js') }}"></script>
+    <!-- main JS
+        ============================================ -->
+    <script src="{{ asset('js/main.js') }}"></script>
+</body>
+
+</html>
