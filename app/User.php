77d713cfa47784fa49fae13e58ebfd8bedb1d01e
dungpham <dung.pham@asiantech.vn>
2020-03-16 23:15:34 +0700

    change class User.php

--- app/User.php
+++ app/User.php
@@ -3,17 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends /*Model*/ \Eloquent implements Authenticatable
+class User extends Authenticatable
 {
-	use AuthenticableTrait;
     protected $fillable = [
- 		'username', 'password', 'email', 'image_url', 'address', 'role'
+ 		'id', 'username', 'password', 'email', 'image_url', 'address', 'role'
     ];
 
     protected $hidden = [
-    	'password', 'remember_token'
+    	'password'
     ];
 }
